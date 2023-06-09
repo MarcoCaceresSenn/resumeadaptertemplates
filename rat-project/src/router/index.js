73aebbb9a7366bbc1e3bc5b2dd3c48fd157acb83
MarcoCaceresSenn <m.caceres04@ufromail.cl>
2023-06-09 14:47:58 -0400

    vista landing router

--- rat-project/src/router/index.js
+++ rat-project/src/router/index.js
@@ -1,26 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import LandingView from '../views/landing-page.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import LandingView from '../views/LandingPage.vue'
+
 
 const routes = [
   {
-    path: '/',
+    path: '/cambiar',
     name: 'landing-page',
     component: LandingView
   },
   {
     path: '/login',
     name: 'login-page',
-    component: () => import('../views/login-page.vue')
+    component: () => import('../views/LoginPage.vue')
   },
   {
     path: '/home',
     name: 'home-page',
-    component: () => import('../views/home-page.vue')
+    component: () => import('../views/HomePage.vue')
+  },
+  {
+    path: '/',
+    name: 'form-resume',
+    component: () => import('../views/FormResume.vue')
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
